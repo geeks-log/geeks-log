9c2abcc703d33d0e2d769fc71860d090d934f8b4
seokju-na <seokju.me@gmail.com>
2019-11-15 21:18:40 +0900

    Update build scripts for ci (#72)
    * Update build commands
    
    * Update rush

---
+++ libraries/ui/scripts/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Lint
+geeks-log-eslint -c .eslintrc.js 'src/**/*.{js,jsx,ts,tsx}'
+
+# Test
+# jest --reporters="jest-standard-reporter"
+
+# Build
+rm -rf dist/
+tsc -p tsconfig.json
