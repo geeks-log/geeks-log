d3e04b1717534e0f82c9bf7a2334c6523e43ac30
seokju-na <seokju.me@gmail.com>
2019-11-17 19:27:23 +0900

    Test builds (#86)
    * Add spinner
    
    * Add changelog

--- libraries/ui/demo/Demo.tsx
+++ libraries/ui/demo/Demo.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Button } from '../src';
+import { Button, Spinner } from '../src';
 
 export default function Demo() {
   return (
     <main>
       <h1>Hello World!</h1>
       <Button>Button</Button>
+      <Spinner />
     </main>
   );
 }
