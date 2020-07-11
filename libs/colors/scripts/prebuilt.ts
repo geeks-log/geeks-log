62c49de67f5a536ec3298c30500c1fedc19e88e0
seokju-na <seokju.me@gmail.com>
2020-07-11 20:02:38 +0900

    Define css variables, add 'adaptive.css' (#15)

--- libs/colors/scripts/prebuilt.ts
+++ libs/colors/scripts/prebuilt.ts
@@ -12,6 +12,8 @@ async function prebuilt() {
   const { styles: lightThemeCssMinified } = await minifier.minify(lightThemeCss);
   const darkThemeCss = createCss('dark');
   const { styles: darkThemeCssMinified } = await minifier.minify(darkThemeCss);
+  const adaptiveCss = createCss();
+  const { styles: adaptiveCssMinified } = await minifier.minify(adaptiveCss);
 
   const lightThemeFilepath = path.join(ROOT_PATH, 'light-theme.css');
   const lightThemeMinifiedFilepath = path.join(ROOT_PATH, 'light-theme.min.css');
@@ -19,11 +21,16 @@ async function prebuilt() {
   const darkThemeFilepath = path.join(ROOT_PATH, 'dark-theme.css');
   const darkThemeMinifiedFilepath = path.join(ROOT_PATH, 'dark-theme.min.css');
 
+  const adaptiveFilepath = path.join(ROOT_PATH, 'adaptive.css');
+  const adaptiveMinifiedFilepath = path.join(ROOT_PATH, 'adaptive.min.css');
+
   await Promise.all([
     fs.remove(lightThemeFilepath),
     fs.remove(lightThemeMinifiedFilepath),
     fs.remove(darkThemeFilepath),
     fs.remove(darkThemeMinifiedFilepath),
+    fs.remove(adaptiveFilepath),
+    fs.remove(adaptiveMinifiedFilepath),
   ]);
 
   await Promise.all([
@@ -31,6 +38,8 @@ async function prebuilt() {
     fs.outputFile(lightThemeMinifiedFilepath, lightThemeCssMinified),
     fs.outputFile(darkThemeFilepath, darkThemeCss),
     fs.outputFile(darkThemeMinifiedFilepath, darkThemeCssMinified),
+    fs.outputFile(adaptiveFilepath, adaptiveCss),
+    fs.outputFile(adaptiveMinifiedFilepath, adaptiveCssMinified),
   ]);
 }
 
--- libs/colors/src/css.ts
+++ libs/colors/src/css.ts
@@ -1,11 +1,14 @@
 import kebabCase from 'lodash.kebabcase';
 import {
+  BackgroundPalette,
   backgroundPalettes,
   ColorPalette,
-  ColorPaletteHue,
   colorPalettes,
   commonColors,
+  ForegroundPalette,
   foregroundPalettes,
+  lightBackgroundPalette,
+  lightForegroundPalette,
 } from './palette';
 
 export function formatCssVariableName(name: string) {
@@ -19,49 +22,116 @@ function keys<T extends { [key: string]: any }>(obj: T) {
   return Object.keys(obj) as Array<keyof T>;
 }
 
-export function createColorPaletteCssVariables(name: string, palette: ColorPalette) {
-  const cssVariables: string[] = [];
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function colorToCss<T extends { [key: string]: any }>(color: T, prefix?: string) {
+  return keys(color).reduce((css, key) => {
+    css[key] = formatCssVariableName(prefix != null ? `${prefix}-${key}` : (key as string));
 
-  const hues = keys(palette).filter((x): x is ColorPaletteHue => x !== 'contrast');
-
-  hues.forEach((hue) => {
-    cssVariables.push(`${formatCssVariableName(name)}-${hue}: ${palette[hue]}`);
-  });
-
-  keys(palette.contrast).forEach((hue) => {
-    cssVariables.push(`${formatCssVariableName(name)}-${hue}-contrast: ${palette.contrast[hue]}`);
-  });
-
-  return cssVariables;
+    return css;
+  }, {} as Record<keyof T, string>);
 }
 
-export function createCss(theme: 'light' | 'dark' = 'light') {
-  let css = ':root {\n';
+function colorPaletteToCss(name: string, colorPalette: ColorPalette) {
+  const { contrast, ...other } = colorPalette;
 
-  const background = backgroundPalettes[theme];
-  const foreground = foregroundPalettes[theme];
+  return {
+    ...colorToCss(other, name),
+    contrast: colorToCss(contrast, `${name}-contrast`),
+  };
+}
+
+type CommonColorsCss = Record<keyof typeof commonColors, string>;
+type ColorPalettesCss = Record<keyof typeof colorPalettes, ColorPalette>;
+
+type Css = Readonly<CommonColorsCss> &
+  Readonly<ColorPalettesCss> & {
+    background: BackgroundPalette;
+    foreground: ForegroundPalette;
+  };
+
+export const css: Css = {
+  ...colorToCss(commonColors),
+  grey: colorPaletteToCss('grey', colorPalettes.grey),
+  red: colorPaletteToCss('red', colorPalettes.red),
+  pink: colorPaletteToCss('pink', colorPalettes.pink),
+  purple: colorPaletteToCss('purple', colorPalettes.purple),
+  blue: colorPaletteToCss('blue', colorPalettes.blue),
+  background: colorToCss(lightBackgroundPalette, 'background'),
+  foreground: colorToCss(lightForegroundPalette, 'foreground'),
+};
+
+export function createCss(theme?: 'light' | 'dark') {
+  let text = ':root {\n';
+
+  const background = backgroundPalettes[theme ?? 'light'];
+  const foreground = foregroundPalettes[theme ?? 'light'];
 
   // 1. common
-  Object.entries(commonColors).forEach(([name, value]) => {
-    css += `  ${formatCssVariableName(`common-${name}`)}: ${value};\n`;
+  keys(commonColors).forEach((key) => {
+    const name = css[key];
+    const value = commonColors[key];
+
+    text += `  ${name}: ${value};\n`;
   });
 
   // 2. colors
-  Object.entries(colorPalettes).forEach(([name, palette]) => {
-    createColorPaletteCssVariables(name, palette).forEach((variable) => {
-      css += `  ${variable};\n`;
+  keys(colorPalettes).forEach((key) => {
+    const palette = css[key];
+    const { contrast, ...other } = palette;
+
+    keys(other).forEach((hue) => {
+      const name = palette[hue];
+      const value = colorPalettes[key][hue];
+
+      text += `  ${name}: ${value};\n`;
+    });
+
+    keys(contrast).forEach((hue) => {
+      const name = palette.contrast[hue];
+      const value = colorPalettes[key].contrast[hue];
+
+      text += `  ${name}: ${value};\n`;
     });
   });
 
   // 3. background, foreground
-  Object.entries(background).forEach(([name, value]) => {
-    css += `  ${formatCssVariableName(`background-${name}`)}: ${value};\n`;
+  keys(background).forEach((key) => {
+    const name = css.background[key];
+    const value = background[key];
+
+    text += `  ${name}: ${value};\n`;
   });
-  Object.entries(foreground).forEach(([name, value]) => {
-    css += `  ${formatCssVariableName(`foreground-${name}`)}: ${value};\n`;
+  keys(foreground).forEach((key) => {
+    const name = css.foreground[key];
+    const value = foreground[key];
+
+    text += `  ${name}: ${value};\n`;
   });
 
-  css += '}';
+  text += '}';
 
-  return css;
+  // Add color preference media query if theme is not specified.
+  if (theme == null) {
+    text += '\n\n';
+    text += `@media (prefers-color-scheme: dark) {\n`;
+    text += `  :root {\n`;
+
+    keys(backgroundPalettes.dark).forEach((key) => {
+      const name = css.background[key];
+      const value = backgroundPalettes.dark[key];
+
+      text += `    ${name}: ${value};\n`;
+    });
+    keys(foregroundPalettes.dark).forEach((key) => {
+      const name = css.foreground[key];
+      const value = foregroundPalettes.dark[key];
+
+      text += `    ${name}: ${value};\n`;
+    });
+
+    text += `  }\n`;
+    text += `}`;
+  }
+
+  return text;
 }
