3f0bbcf636cf6efbd0ab4efdf637e329ebc17200
seokju-na <seokju.me@gmail.com>
2020-07-11 17:28:55 +0900

    Lazy import when create babel config

--- tools/babel/index.js
+++ tools/babel/index.js
@@ -1,32 +1,27 @@
-const defaultConfig = {
-  presets: [
-    [
-      require('@babel/preset-env'),
-      {
-        useBuiltIns: 'entry',
-        corejs: 3,
-      },
-    ],
-  ],
-  plugins: [
-    [require('@babel/plugin-proposal-class-properties'), { loose: true }],
-    require('@babel/plugin-proposal-optional-chaining'),
-    require('@babel/plugin-proposal-nullish-coalescing-operator'),
-    require('@babel/plugin-proposal-numeric-separator'),
-  ],
-};
-
 module.exports = {
   createConfig(options = {}) {
-    const { withReact = false, withTypescript = true, withEmotion = false, extraConfigs } = options;
-    const config = { ...defaultConfig };
+    const { withReact = false, withEmotion = false, extraConfigs } = options;
+    const config = {
+      presets: [
+        require('@babel/preset-typescript'),
+        [
+          require('@babel/preset-env'),
+          {
+            useBuiltIns: 'entry',
+            corejs: 3,
+          },
+        ],
+      ],
+      plugins: [
+        [require('@babel/plugin-proposal-class-properties'), { loose: true }],
+        require('@babel/plugin-proposal-optional-chaining'),
+        require('@babel/plugin-proposal-nullish-coalescing-operator'),
+        require('@babel/plugin-proposal-numeric-separator'),
+      ],
+    };
 
     if (withReact) {
       config.presets.push(require('@babel/preset-react'));
-    }
-
-    if (withTypescript) {
-      config.presets.push(require('@babel/preset-typescript'));
     }
 
     if (withEmotion) {
